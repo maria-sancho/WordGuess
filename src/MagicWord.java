272ac3cd695c6720b7b1a5ff20caa4996bdb968b
maria-sancho <alusan7934@ieselcaminas.org>
2023-02-02 14:17:09 +0100

    WordGuess

--- src/MagicWord.java
+++ src/MagicWord.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class MagicWord {
+public class MagicWord {
+    private String word;
+    private String state;
+    private boolean[] revealed;
+    private boolean guessed;
+
+    public MagicWord(String word) {
+        this.word = word;
+        this.state = "";
+
+        for (int i = 0; i < word.length(); i++) {
+            this.state += "_";
+        }
+        this.revealed = new boolean[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            this.revealed[i] = false;
+        }
+        this.guessed = false;
+    }
+
+    public String getWord() {
+        return this.word;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public boolean isGuessed() {
+        return this.guessed;
+    }
+
+    public void updateState(char c) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == c) {
+                revealed[i] = true;
+            }
+        }
+
+        this.state = "";
+        for (int i = 0; i < word.length(); i++) {
+            if (revealed[i]) {
+                this.state += word.charAt(i);
+            } else {
+                this.state += "_";
+            }
+        }
+
+        boolean allRevealed = true;
+        for (int i = 0; i < word.length(); i++) {
+            if (!revealed[i]) {
+                allRevealed = false;
+                break;
+            }
+        }
+
+        if (allRevealed) {
+            this.guessed = true;
+        }
+    }
+
+    public void setGuessed(boolean guessed) {
+        this.guessed = guessed;
+    }
 }
--- src/WordGuess.java
+++ src/WordGuess.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class WordGuess {
+import java.util.Random;
+import java.util.Scanner;
+
+public class WordGuess {
+    private static final String[] WORDS = {"apple", "banana", "grape", "orange", "strawberry"};
+
+    private MagicWord magicWord;
+
+    public WordGuess() {
+        Random rand = new Random();
+        String word = WORDS[rand.nextInt(WORDS.length)];
+        magicWord = new MagicWord(word);
+    }
+
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        int attempts = 0;
+
+        while (!magicWord.isGuessed()) {
+            System.out.println("Attempt " + (attempts + 1) + ": " + magicWord.getState());
+            System.out.print("Enter one character or your guess word: ");
+            String input = scanner.nextLine();
+
+            if (input.length() == 1) {
+                magicWord.updateState(input.charAt(0));
+            } else {
+                if (input.equals(magicWord.getWord())) {
+                    magicWord.setGuessed(true);
+                }
+            }
+            attempts++;
+        }
+
+        System.out.println("Congratulation! You got it in " + attempts + " attempts.");
+    }
+
 }
